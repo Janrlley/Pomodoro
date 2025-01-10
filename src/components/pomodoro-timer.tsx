e8866556d0a2ae537df4db4f6f03424b2946e944
Janrlley <105364286+Janrlley@users.noreply.github.com>
2025-01-10 12:37:13 -0300

    Commit error pomodoro-timer

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
 import { Timer } from './timer';
